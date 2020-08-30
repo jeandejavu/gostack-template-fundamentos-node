365755bd329fda3dba6bafc3a8e84a06798012b6
Jean Melo <jeandejavu@hotmail.com>
2020-08-30 10:42:53 -0300

    feat: implementacoes recursos transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,34 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.send({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
-transactionRouter.post('/', (request, response) => {
+transactionRouter.post('/', ({ body: { title, value, type } }, response) => {
   try {
-    // TODO
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+    return response.send(
+      createTransactionService.execute({ title, value, type }),
+    );
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
